5b7c70808c30fb5ab18aad6b263f211bb092aa71
d.timofeev <Dima27.07.90@mail.ru>
2019-10-17 21:02:49 +0300

    Удалил карусельку

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
 "use strict";
-
-$(document).ready(function () {
-
-    var carousel = $("#carousel");
-
-    carousel.owlCarousel();
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
